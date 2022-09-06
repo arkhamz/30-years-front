9db33396bd388e9efd5423b0d107718db3b1418a
arkhamz <shile2007@gmail.com>
2022-09-06 11:42:02 +0200

    routes and page components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+//pages & components
+import Atlas from "./pages/Atlas";
+import Home from "./pages/Home";
+import BattleDetail from "./pages/BattleDetail";
+import Commanders from "./pages/Commanders";
+import CommanderDetail from "./pages/CommanderDetail";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/atlas" element={Atlas} />
+        <Route path="/battles/:id" element={Atlas} />
+        <Route path="/commanders" element={Commanders} />
+        <Route path="/commanders/:id" element={CommanderDetail} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul className="links"></ul>
+    </nav>
+  );
+}
+
+export default Navbar;
