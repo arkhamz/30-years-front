b0feaf8ede33a9b12d8ebd41d6f25fdaf0bda56a
arkhamz <shile2007@gmail.com>
2022-09-07 11:08:56 +0200

    set up redux and battle fetching on battle page2

--- src/pages/BattleDetail.js
+++ src/pages/BattleDetail.js
@@ -1,4 +1,22 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectOneBattle } from "../store/battle/battleSelectors";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+import { fetchOneBattle } from "../store/battle/battleThunks";
+
 function BattleDetail() {
+  const { id } = useParams();
+  const singleBattle = useSelector(selectOneBattle);
+  const dispatch = useDispatch();
+
+  useEffect(function () {
+    //dispatch thunk action to get battle
+    dispatch(fetchOneBattle(id));
+  }, []);
+
+  console.log(singleBattle);
+
   return (
     <div>
       <h1>BattleDetail</h1>
--- src/store/battle/battleThunks.js
+++ src/store/battle/battleThunks.js
@@ -8,7 +8,6 @@ export function fetchBattles() {
       const response = await axios.get(`http://localhost:4000/battles`);
       const battlesArr = response.data;
       dispatch(storeBattles(battlesArr));
-      console.log(battlesArr);
     } catch (e) {
       console.log(e);
       console.log(e.message);
@@ -20,8 +19,9 @@ export function fetchOneBattle(id) {
   return async function (dispatch, getState) {
     try {
       //GET REQUEST /battles/:id
-      const response = await axios.get(`http://localhost:4000/battles${id}`);
+      const response = await axios.get(`http://localhost:4000/battles/${id}`);
       const battle = response.data;
+      dispatch(storeSingleBattle(battle));
     } catch (e) {
       console.log(e);
       console.log(e.message);
