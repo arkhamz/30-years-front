6a4696f365705a4db58e7d455c4aa3bae805e0ae
arkhamz <shile2007@gmail.com>
2022-09-12 17:25:20 +0200

    media queries and styling

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,49 +1,58 @@
-import "./Login.css"
-import {useState} from "react"
+import "./Login.css";
+import { useState } from "react";
 import { login } from "../store/user/userThunks";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-function Login(){
-    
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const dispatch = useDispatch();
-    const navigator = useNavigate()
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigator = useNavigate();
 
-    function handleSubmit(e){
-        e.preventDefault();
+  function handleSubmit(e) {
+    e.preventDefault();
 
-        dispatch(login(email,password,navigator));
+    dispatch(login(email, password, navigator));
 
-        //clear fields
-        setEmail("");
-        setPassword("");
-  
-        //thunk will handle the re-direct  
-    }
+    //clear fields
+    setEmail("");
+    setPassword("");
 
-    return(
-        <div className="login-container">
-            <form className="login-form" onSubmit={e => e.preventDefault()} >
-                <div className="login-email">
-                    <label htmlFor="login-email">Email:</label>
-                    <input id="login-email" value={email} onChange={e => setEmail(e.target.value)} required type="text" />
-                </div>
-                <div className="listing-form-bedroom">
-                    <label htmlFor="login-password">Password:</label>
-                    <input id="login-password" value={password} onChange={e => setPassword(e.target.value)}  required type="password" />
+    //thunk will handle the re-direct
+  }
 
-                </div>
-               
-                <div className="login-form-button">
-                    <button onClick={handleSubmit}>Login</button>
-                </div>
-            </form>
-         
+  return (
+    <div className="login-container">
+      <h1>Login</h1>
+      <form className="login-form" onSubmit={(e) => e.preventDefault()}>
+        <div className="login-email">
+          <label htmlFor="login-email">Email:</label>
+          <input
+            id="login-email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            type="text"
+          />
         </div>
-    )
+        <div className="listing-form-bedroom">
+          <label htmlFor="login-password">Password:</label>
+          <input
+            id="login-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            type="password"
+          />
+        </div>
 
+        <div className="login-form-button">
+          <button onClick={handleSubmit}>Login</button>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,55 +1,73 @@
-import "./Signup.css"
-import {useState} from "react"
+import "./Signup.css";
+import { useState } from "react";
 import { signup } from "../store/user/userThunks";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 
-function Signup(){
-  const [username,setUsername] = useState("");
-  const [email,setEmail] = useState("");
-  const [password,setPassword] = useState("");
+function Signup() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const dispatch = useDispatch();
   const navigator = useNavigate();
 
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch(signup(email, password, username, navigator));
 
-  function handleSubmit(e){
-      e.preventDefault();
-      dispatch(signup(email,password,username,navigator));
+    //clear fields
+    setUsername("");
+    setEmail("");
+    setPassword("");
 
-      //clear fields
-      setUsername("");
-      setEmail("");
-      setPassword("");
-  
-      //thunk will handle the re-direct  
+    //thunk will handle the re-direct
   }
 
-    return(
-        <div className="signup-container">
-            <form className="signup-form" onSubmit={e => e.preventDefault()} >
-                <div className="signup-username">
-                        <label htmlFor="signup-username">Username:</label>
-                        <input id="signup-username" value={username} onChange={e => setUsername(e.target.value)} required type="text" />
-                    </div>
-                <div className="signup-email">
-                    <label htmlFor="signup-email">Email:</label>
-                    <input id="signup-email" value={email} onChange={e => setEmail(e.target.value)} required type="text" />
-                </div>
-                <div className="listing-form-bedroom">
-                    <label htmlFor="signup-password">Password:</label>
-                    <input id="signup-password" value={password} onChange={e => setPassword(e.target.value)}  required type="password" />
-
-                </div>
-               
-                <div className="signup-form-button">
-                    <button onClick={handleSubmit}>Signup</button>
-                </div>
-            </form>
-         
+  return (
+    <div className="signup-container">
+      <h1>Create an Account</h1>
+      <form className="signup-form" onSubmit={(e) => e.preventDefault()}>
+        <div className="signup-username">
+          <label htmlFor="signup-username">Username:</label>
+          <input
+            id="signup-username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            type="text"
+          />
         </div>
-    )
+        <div className="signup-email">
+          <label htmlFor="signup-email">Email:</label>
+          <input
+            id="signup-email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+            type="text"
+          />
+        </div>
+        <div className="listing-form-bedroom">
+          <label htmlFor="signup-password">Password:</label>
+          <input
+            id="signup-password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            type="password"
+          />
+        </div>
 
+        <div className="signup-form-button">
+          <button onClick={handleSubmit}>Signup</button>
+          <Link to="/login">
+            Already have an account? <span>Login</span>
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
 }
 
 export default Signup;
