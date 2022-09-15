7f3fd54b843f56770800624c34ec7ce98424a976
arkhamz <shile2007@gmail.com>
2022-09-15 21:11:18 +0200

    comments to progress status icons function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -41,12 +41,15 @@ function Navbar() {
   }
 
   function statusIcons() {
-    let iconsArr = [];
-
+    let iconsArr = []; //will contain array of {component,class}
+    //max items are 12
     for (let i = 1; i < 13; i++) {
+      //if i/number less than or equal to progress, add icon object with status-light class
+      //i.e. if you have unlocked it
       if (progress && i <= progress) {
         iconsArr.push({ Component: GiCrossedSwords, class: "status-light" });
       } else if (progress && i > progress) {
+        // if i is greater than progress, i.e. u haven't unlocked it, 
         iconsArr.push({ Component: GiCrossedSwords, class: "status-dark" });
       }
     }
