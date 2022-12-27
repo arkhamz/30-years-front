a16f7f0acff81a6e6eec25a73aa61d68d66aea97
Shile Balogun <68877605+arkhamz@users.noreply.github.com>
2022-12-27 12:58:34 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,4 @@
 // ---PRODUCTION--
-// export const API_URL = "https://history-matex2.onrender.com"
-// export const API_URL = "https://history-mate-api.herokuapp.com"
-//export const API_URL = "https://history-mate-api.herokuapp.com" heroku free disabled
-export const API_URL = "https://history-matex2.onrender.com"
+export const API_URL = "https://history-matex2.onrender.com";
 // ---DEV---
 // export const API_URL = "http://localhost:4000"
